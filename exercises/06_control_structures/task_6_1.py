d893987c89eba6914ca737218db545455c02df3b
root <root@zelentsov>
2024-11-26 21:15:28 +0500

    task 6_1

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -13,3 +13,9 @@ MAC-адреса используются в формате XXXX.XXXX.XXXX
 """
 
 mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+
+result = []
+for word in mac:
+    result.append(word.replace(':','.'))
+
+print(result)
