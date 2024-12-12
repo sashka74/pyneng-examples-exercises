51f3c578c07964ffc090e99ddab3bc18c5474a5b
Zelentsov.AS <azelentsov@zelentsov.INFINNITY.LOCAL>
2024-12-12 11:31:29 +0500

    task 6_2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,36 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip = input(f"Введите IP-адрес в формате 10.0.1.1: ")
+
+
+
+try:
+    ip_list = ip.split('.')
+    flag = False
+    if len(ip_list) != 4:
+        raise ValueError("Неправильный IP-адрес")
+
+    for i in ip_list:
+        if not i.isnumeric() or int(i) not in range(0,256):
+            print("Неправильный IP-адрес")
+            flag = True
+            break
+
+    if flag == False:
+        broadcast = '255.255.255.255'
+        first_byte = int(ip.split('.')[0])
+        if 0 < first_byte < 224:
+            print ('unicast')
+        elif 223 < first_byte < 240:
+            print('multicast')
+        elif ip == broadcast:
+            print('local broadcast')
+        elif ip == '0.0.0.0':
+            print ('unassigned')
+        else:
+            print ('unused')
+
+except ValueError:
+    print("Неправильный IP-адрес")
